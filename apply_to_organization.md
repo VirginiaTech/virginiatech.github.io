f51f808667b68c5b3b63e6e91389fc46936a8bfa
forteddyt <forteddyt@gmail.com>
2017-02-13 15:59:56 -0500

    Determines if GitHub user is valid

--- apply_to_organization.md
+++ apply_to_organization.md
@@ -4,11 +4,15 @@ permalink: /apply_to_organization
 ---
 
 <b>Email</b> <font size="2.5rem"> (required) </font><br>
-<input class="rounded" type="email" name="_replyto" placeholder="" id="email_field" required>@vt.edu<br><br>
+<input class="rounded" type="username" name="_replyto" placeholder="" id="email_field" required>@vt.edu<br><br>
 <b>GitHub Username</b> <font size="2.5rem"> (required) </font><br>
-<input class="rounded" tpye="username" name="subject" placeholder="" id="username_field" required><br><br>
+<input class="rounded" type="username" name="subject" placeholder="" id="username_field" required><br><br>	
 <input type="submit" value="Apply" onclick="tempFunc();">
 <input type="hidden" name="_next" value="{{ site.baseurl }}{% link submit_success.md %}" />
 
+<script
+  src="https://code.jquery.com/jquery-3.1.1.min.js"
+  integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
+  crossorigin="anonymous"></script>
 <script type="text/javascript" src="assets/javascript/temp.js"></script>
 <script type="text/javascript" src="assets/javascript/verifyUsername.js"></script>
--- assets/javascript/verifyUsername.js
+++ assets/javascript/verifyUsername.js
@@ -1,6 +1,26 @@
 var searchTimeout;
+var lastLength;
 var formUsername = document.querySelector("#username_field");
+var bgColor = document.getElementById("username_field").style.backgroundColor;
 
+/* ---For development purposes only--- */
+var queryString = "";
+var token = "";
+
+function getToken(){
+    return $.getJSON("data/OAuthTokens.json", function(data){
+        token = data.Organization_Read_Access;
+    })
+}
+
+function setQueryString(){
+    queryString = "?access_token=" + token;
+}
+
+$.when(getToken()).then(setQueryString);
+/* ----------------------------------- */
+
+var userVal = 0;
 document.getElementById("username_field").onkeydown = function() {
     if (searchTimeout != undefined) {
         clearTimeout(searchTimeout);
@@ -9,22 +29,37 @@ document.getElementById("username_field").onkeydown = function() {
 };
 
 function callServerScript() {
+    if (lastLength == formUsername.value.length || formUsername.value.length <= 0) {
+        if(formUsername.value.length <= 0){
+            userVal = 0;
+        }
 
-    if (formUsername.value.length <= 0) {
+        if(userVal > 1){
+            document.getElementById("username_field").style.backgroundColor= "#e6ffe6";
+        }else if(userVal == 0){
+            document.getElementById("username_field").style.backgroundColor= "";
+        }
+        else if(userVal < 0) {
+            document.getElementById("username_field").style.backgroundColor= "#ffb3b3";
+        }
         return;
+    }else{
+        lastLength = formUsername.value.length;
     }
 
     var pageReq = new XMLHttpRequest();
 
-    console.log("length is: " + formUsername.value.length);
-
     pageReq.timeout = 2000;
+
     pageReq.onreadystatechange = function(e) {
         if (pageReq.readyState == 4) {
             if (pageReq.status == 200) {
-                console.log("A User!");
+                document.getElementById("username_field").style.backgroundColor= "#e6ffe6";
+                document.getElementById("username_field").style.outlineColor= "#606C71";
+                userVal = 1;
             } else {
-                console.log("Not a user");
+                document.getElementById("username_field").style.backgroundColor= "#ffb3b3";
+                userVal = -1;
             }
         }
     }
@@ -33,6 +68,10 @@ function callServerScript() {
         console.error("Request timeout: ", pageReq);
     }
 
-    pageReq.open("GET", "https://api.github.com/users/" + formUsername.value, true);
+    var url = "https://api.github.com/users/" + formUsername.value;
+    url = url + queryString;
+
+    pageReq.open("GET", url, true);
     pageReq.send();
 }
+
