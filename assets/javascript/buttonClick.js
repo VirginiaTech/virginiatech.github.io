ef134790dea2a1145d4d6476a441765cec5b2e37
forteddyt <forteddyt@gmail.com>
2017-03-31 18:30:01 -0400

    Pre-reqs for new featured-repo system

--- assets/javascript/buttonClick.js
+++ assets/javascript/buttonClick.js
@@ -7,6 +7,9 @@ Name will probably change as more methods are added.
 
 function openClick(url, newTab = false){
 	ctrlKeyPressed = event.ctrlKey;
+	if(!url){
+		return;
+	}
 	
 	if(!ctrlKeyPressed && !newTab){
 		window.location.href = url;
