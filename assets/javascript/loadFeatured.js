fb277fe6a309fb45ba56a90d7b7dc2e89da3d131
forteddyt <forteddyt@gmail.com>
2017-04-24 08:54:51 -0400

    comment removal

--- assets/javascript/loadFeatured.js
+++ assets/javascript/loadFeatured.js
@@ -7,7 +7,7 @@ f5 = document.getElementById("featured_5");
 function loadJSON(callback) {   
     var xobj = new XMLHttpRequest();
         xobj.overrideMimeType("application/json");
-    xobj.open('GET', 'FeaturedUsers/fData.json', true); // Replace 'my_data' with the path to your file
+    xobj.open('GET', 'FeaturedUsers/fData.json', true);
     xobj.onreadystatechange = function () {
           if (xobj.readyState == 4 && xobj.status == "200") {
             // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
